d904c1e3e131eeb6440b208899617836936e7f14
Ezequiel M. Izaguirre <ezequielmizaguirre@gmail.com>
2024-02-07 19:26:14 -0300

    Prueba inicial con Tailwind

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,113 +1,88 @@
+'use client'
 import Image from "next/image";
+import {FormEvent, useState} from "react"
+import {redirect} from 'next/navigation'
 
-export default function Home() {
+//Pestaña principal de inicio de sesión
+
+export default function Login() 
+{
+  let [user, setUser] = useState("")
+  let [pass, setPass] = useState("")
+
+  let onChange = function(event : React.SyntheticEvent)
+  {
+    let target = event.target as HTMLInputElement
+    switch(target.id)
+    {
+      case "user":
+        setUser(target.value)
+        break;
+      case "pass":
+        setPass(target.value)
+        break;
+    }
+  }
+
+  let onSubmit = function(event : React.SyntheticEvent)
+  {
+    event.preventDefault()  //Necesario en formularios que usan un input type="submit"
+
+    alert(`Has iniciado sesión con usuario: ${user} y contraseña: ${pass}`)
+
+    if(user && pass)  //Si no son null, undefined, empty string, false, etc | [Falsy]
+    {
+      //validateLogin(user,pass).then()
+      redirect("/home")
+    }
+
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+  <main className="bg-gray-900 h-screen w-screen flex items-center">
+  <div className="w-fit h-fit text-slate-200 my-6 mx-auto min-w-[300px]">
+    <header className="mx-auto text-center py-4">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/512px-Tailwind_CSS_Logo.svg.png?20230715030042" className="w-10 mx-auto" />
+      <h1 className="font-bold mt-2">Bienvenido a Marketshared</h1>
+      <h2 className="text-slate-400">Ingresa con tu cuenta</h2>
+    </header>
+
+    <form onSubmit={onSubmit} className="bg-gray-900 w-fit mx-auto rounded-lg p-3">
+      
+      <label htmlFor="user" className="block font-semibold">Usuario</label>
+      <input type="text" id="user" onChange={onChange} className="border 
+      rounded-md py-1 px-1  w-full md:w-[350px] bg-gray-800 border-slate-600 mt-2"/>
+
+      <div className="flex items-center mt-4">
+        <label htmlFor="pass" className="block font-semibold flex-1">Contraseña</label>
+        <a className="text-sm font-semibold 
+        text-teal-400 hover:text-teal-300 ms-8" href="#">Olvidé mi contraseña</a>
       </div>
+      <input type="password" id="pass" onChange={onChange} className="border 
+      rounded-md py-1 px-1 w-full md:w-[350px] bg-gray-800 border-slate-600 mt-2"/>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <input className="block mt-6 bg-blue-900 py-2 px-6 rounded-xl font-semibold
+        hover:bg-blue-700 w-full" type="submit" value="Iniciar sesión"/>
+
+    </form>
+
+    <section className="text-sm text-center mt-4">
+      <h1 className="text-slate-400">¿No sos miembro?<a className="font-semibold 
+          text-teal-400 hover:text-teal-300 my-1 block" href="#">Crea una nueva cuenta</a>¡Es gratis!</h1>
+      <div className="m-4 border-t border-slate-600 p-2">
+        <h1 className="font-semibold">Marketshared es la red de ventas B2B más usada del mundo</h1>
+        <p className="text-slate-400">Descubre miles de productos y negocia con las empresas más importantes del mundo</p>
       </div>
+    </section>
+  </div>
+  </main>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
+
+  async function validateLogin(user : String, pass : String)
+  {
+      let result = await fetch("localhost/3000/api...")
+      return result.ok
+  }
+
 }
